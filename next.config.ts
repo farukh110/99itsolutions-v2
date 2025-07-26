d3ff2414e795ffaa34169c69680ce732decd6690
farukh110 <farukhsajjad110@gmail.com>
2025-07-26 17:42:08 +0500

    updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "play-lh.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "is1-ssl.mzstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "abadraho.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
