b0cec056073bd7da64d4571a15fe8c9f82e14732
farukh110 <farukhsajjad110@gmail.com>
2025-07-27 17:09:42 +0500

    added meta titles and description

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,16 @@
-'use client';
+// 'use client';
 
 import React from 'react';
-import { useRouter } from 'next/navigation';
+import { Metadata } from 'next';
+import Link from 'next/link';
 import './not-found.scss';
 
-const NotFound: React.FC = () => {
+export const metadata: Metadata = {
+    title: "Error 404",
+    description: "Error 404",
+};
 
-    const router = useRouter();
+const NotFound: React.FC = () => {
 
     return (
         <section className="not-found-container py-30 pb-5 rpy-140 pt-4 shadow">
@@ -19,12 +23,19 @@ const NotFound: React.FC = () => {
                         <p className="text-center">
                             It seems the page you&apos;re looking for wandered off. Let&apos;s get you back on track!
                         </p>
-                        <button
-                            onClick={() => router.push('/')}
-                            className="btn btn-primary mb-md-5 mb-lg-5 mb-xl-5 mb-5 btn-connect mx-auto d-block"
-                        >
-                            Go to Home
-                        </button>
+
+                        <div className='row justify-content-center'>
+
+                            <div className='col-md-2 col-6'>
+                                <Link
+                                    href="/"
+                                    className="btn btn-primary mb-md-5 mb-lg-5 mb-xl-5 mb-5 btn-connect mx-auto d-block"
+                                >
+                                    Go to Home
+                                </Link>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
